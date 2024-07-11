49d0fa6f5c33dc09cdc8180c8dea016522ec07ba
jezrelljolampong <jezrelljolampong17@gmail.com>
2024-07-11 09:21:39 -0600

    initial setup

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,173 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+         Wate Billing App
+        </q-toolbar-title>
+
+        <div>Logout</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+    <q-list bordered padding class=" text-primary">
+      <q-item
+        clickable
+        v-ripple
+        :active="link === 'inbox'"
+        @click="link = 'inbox'"
+        active-class="my-menu-link"
+      >
+        <q-item-section avatar>
+          <q-icon name="inbox" />
+        </q-item-section>
+
+        <q-item-section>Inbox</q-item-section>
+      </q-item>
+
+      <q-item
+        clickable
+        v-ripple
+        :active="link === 'outbox'"
+        @click="link = 'outbox'"
+        active-class="my-menu-link"
+      >
+        <q-item-section avatar>
+          <q-icon name="send" />
+        </q-item-section>
+
+        <q-item-section>Outbox</q-item-section>
+      </q-item>
+
+      <q-item
+        clickable
+        v-ripple
+        :active="link === 'trash'"
+        @click="link = 'trash'"
+        active-class="my-menu-link"
+      >
+        <q-item-section avatar>
+          <q-icon name="delete" />
+        </q-item-section>
+
+        <q-item-section>Trash</q-item-section>
+      </q-item>
+
+      <q-separator spaced />
+
+      <q-item
+        clickable
+        v-ripple
+        :active="link === 'settings'"
+        @click="link = 'settings'"
+        active-class="my-menu-link"
+      >
+        <q-item-section avatar>
+          <q-icon name="settings" />
+        </q-item-section>
+
+        <q-item-section>Settings</q-item-section>
+      </q-item>
+
+      <q-item
+        clickable
+        v-ripple
+        :active="link === 'help'"
+        @click="link = 'help'"
+        active-class="my-menu-link"
+      >
+        <q-item-section avatar>
+          <q-icon name="help" />
+        </q-item-section>
+
+        <q-item-section>Help</q-item-section>
+      </q-item>
+    </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+const linksData = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData,
+      link: 'inbox'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.my-menu-link {
+  color: white;
+  background: #F2C037;
+}
+</style>
